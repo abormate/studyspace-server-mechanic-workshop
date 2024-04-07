7d2af875816e0caca6f1483e7d255c0a5f017f77
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-08 05:02:31 +0800

    K8s LearnSpace -- Intro 101

--- k8s-kubernetes-learnspace/boot-dev-k8s/concepts-fundamentals/00-what-is-k8s.sh
+++ k8s-kubernetes-learnspace/boot-dev-k8s/concepts-fundamentals/00-what-is-k8s.sh
@@ -17,5 +17,8 @@ Kubernetes orchestrates and manages collections of containers (often
 those created by Docker). It takes care of scaling, distribution, and connectivity among these containers. 
 Think of it as a system to manage many containers and the infrastructure they run on.
 
+For example, you could install Docker on a single server, and route traffic directly to it. That's fairly simple to set up, 
+but what if you want 10 instances of that server? What about 1000 instances? What if you want to deploy many different services, 
+each scaling up with more instances depending on load? Those are the problems that Kubernetes solves.
 
 '
