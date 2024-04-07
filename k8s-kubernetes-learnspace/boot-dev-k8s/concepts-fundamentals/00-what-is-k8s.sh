7db072793c134a159bef2e946a089031c59dcb4a
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-08 04:56:28 +0800

    K8s LearnSpace -- Intro Concepts

--- k8s-kubernetes-learnspace/boot-dev-k8s/concepts-fundamentals/00-what-is-k8s.sh
+++ k8s-kubernetes-learnspace/boot-dev-k8s/concepts-fundamentals/00-what-is-k8s.sh
@@ -4,3 +4,10 @@
 #
 # ----------------------------------------------- #
 
+: '
+Kubernetes, also known as K8s, is an open-source system for automating 
+deployment, scaling, and management of containerized applications.
+
+-- The Kubernetes team
+
+'
