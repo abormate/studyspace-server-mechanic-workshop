89e785bd073cd14e8565790849968887562f9f39
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-08 04:57:24 +0800

    LearnSpace K8s -- Intro Concepts

--- k8s-kubernetes-learnspace/boot-dev-k8s/concepts-fundamentals/00-what-is-k8s.sh
+++ k8s-kubernetes-learnspace/boot-dev-k8s/concepts-fundamentals/00-what-is-k8s.sh
@@ -11,3 +11,11 @@ deployment, scaling, and management of containerized applications.
 -- The Kubernetes team
 
 '
+
+: '
+Kubernetes orchestrates and manages collections of containers (often 
+those created by Docker). It takes care of scaling, distribution, and connectivity among these containers. 
+Think of it as a system to manage many containers and the infrastructure they run on.
+
+
+'
