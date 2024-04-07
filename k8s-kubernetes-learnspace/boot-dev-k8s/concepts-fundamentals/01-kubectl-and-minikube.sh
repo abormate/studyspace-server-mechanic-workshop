6d326e80f98f6b553a614e033a656739ca654dd0
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-08 06:03:52 +0800

    K8s LearnSpace -- Minikube

--- k8s-kubernetes-learnspace/boot-dev-k8s/concepts-fundamentals/01-kubectl-and-minikube.sh
+++ k8s-kubernetes-learnspace/boot-dev-k8s/concepts-fundamentals/01-kubectl-and-minikube.sh
@@ -4,3 +4,9 @@
 #
 # ---------------------------------------------- #
 
+: '
+We will be using Minikube to practice with Kubernetes. In production, you probably wouldnt use Minikube, you would use a 
+cluster of servers, probably in the cloud. Thats expensive! Minikube is a fantastic tool that allows you to run a 
+single-node Kubernetes cluster on your local machine.
+
+'
