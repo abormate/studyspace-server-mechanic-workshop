5d88832a0986f81139d354b81b50a6afdcd08e8a
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-02 19:41:49 +0800

    101 -- C++ Programming -- Compile

--- server-side/codecademy-c++/oct-2024/02-compile-run.cpp
+++ server-side/codecademy-c++/oct-2024/02-compile-run.cpp
@@ -5,3 +5,17 @@ int main() {
   std::cout << "Codecademy\n";
   
 }
+
+// ------------------------------- //
+
+/*
+Compile with. 
+
+g++ <filename>.cpp 
+
+This will output a file called "a.out"
+
+
+
+
+*/
