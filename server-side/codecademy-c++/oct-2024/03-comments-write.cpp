30cf51e49013c4c388c3b143c7a907301e778e94
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-08 01:49:00 +0800

    101 -- C++ Programming Crash Course -- Documentation

---
+++ server-side/codecademy-c++/oct-2024/03-comments-write.cpp
@@ -0,0 +1,12 @@
+// ------------------------------------- //
+//
+// -- Writing Comments -- Documentation --
+//
+// ------------------------------------- //
+
+/*
+Programming is often highly collaborative. In addition, our own code can quickly become difficult to 
+understand when we return to it — sometimes only a few hours later! For these reasons, it’s often useful 
+to leave notes in our code for ourselves or other developers.
+
+*/
