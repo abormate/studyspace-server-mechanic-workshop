98f9221dc4b43326c5375aa791aff32ad92a99be
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-08 01:54:53 +0800

    101 -- C++ Programming Fundamentals -- LearnSpace

--- server-side/codecademy-c++/oct-2024/03-comments-write.cpp
+++ server-side/codecademy-c++/oct-2024/03-comments-write.cpp
@@ -9,4 +9,16 @@ Programming is often highly collaborative. In addition, our own code can quickly
 understand when we return to it — sometimes only a few hours later! For these reasons, it’s often useful 
 to leave notes in our code for ourselves or other developers.
 
+As we write a C++ program, we can write comments in the code that the compiler will ignore as our program runs.
+
 */
+
+// Write error message
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Please check server default config\n";
+  
+}
