e231ab5c0782097f33f01ffd521fa1a6be6cd36e
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-09 21:00:56 +0800

    101 -- C++ Programming -- Foundational -- Var Storage

---
+++ server-side/codecademy-c++/oct-2024/05-value-storage-var.cpp
@@ -0,0 +1,21 @@
+// --------------------------------- //
+//
+// -- C++ Programming -- Variables --
+//
+// --------------------------------- //
+
+/*
+In C++, variable names consist only of upper/lower 
+case letters, digits, and/or underscores.
+
+*/
+
+#include <iostream>
+
+int main() 
+{
+
+  // Declare a variable
+  
+  
+}
