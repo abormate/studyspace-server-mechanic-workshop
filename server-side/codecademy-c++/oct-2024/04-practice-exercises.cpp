3b55eb028c70dda487c5417b963cc5e8c85bf4a7
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-09 19:59:24 +0800

    101 -- C++ Programming -- Exercise 01

--- server-side/codecademy-c++/oct-2024/04-practice-exercises.cpp
+++ server-side/codecademy-c++/oct-2024/04-practice-exercises.cpp
@@ -4,3 +4,17 @@
 //
 // ------------------------------------------ //
 
+/*
+
+
+
+*/
+
+// Hey. I'm learning how to write C++. Apart from this, I also love food and coffee. 
+
+#include <iostream>
+
+int main() {
+  // comment --
+
+}
