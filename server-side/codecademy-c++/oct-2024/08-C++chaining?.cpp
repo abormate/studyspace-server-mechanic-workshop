73d5be123f48ca1ccb7b461a2d0a97f6f9423e61
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-18 01:34:26 +0800

    101 -- C++ Programming -- Chaining

---
+++ server-side/codecademy-c++/oct-2024/08-C++chaining?.cpp
@@ -0,0 +1,13 @@
+// ----------------------------------- //
+//
+// -- C++ Programming -- Print Resulting Variable
+//
+// ----------------------------------- //
+
+/*
+Now that we have outputted a variable and have also outputted things using multiple couts, let’s take a closer look at cout again.
+
+If we have the code below:
+
+*/
+
