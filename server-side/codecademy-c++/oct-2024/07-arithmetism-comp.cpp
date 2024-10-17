4845c2da16ca72db192abe3fc1a778fe28354710
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-18 00:51:26 +0800

    101 -- C++ Programming -- Arithmetic Compute

--- server-side/codecademy-c++/oct-2024/07-arithmetism-comp.cpp
+++ server-side/codecademy-c++/oct-2024/07-arithmetism-comp.cpp
@@ -4,3 +4,9 @@
 //
 // ---------------------------------- //
 
+/*
+Computers are incredible at doing calculations. Now that we have declared 
+Preview: Docs A variable refers to a storage location in the computer’s memory that one can set aside to save, retrieve, and manipulate data. To create a variable, the type must be specified and a value must be assigned to it.
+variables, let’s use them with arithmetic operators to calculate things!
+
+*/
