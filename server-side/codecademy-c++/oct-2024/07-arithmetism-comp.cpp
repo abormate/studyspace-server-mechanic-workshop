f263cc44ae9a6469c2c89327d85ffa6d7bbb16d4
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-18 00:56:21 +0800

    101 -- C++ Programming -- Arithmetic Computation

--- server-side/codecademy-c++/oct-2024/07-arithmetism-comp.cpp
+++ server-side/codecademy-c++/oct-2024/07-arithmetism-comp.cpp
@@ -1,13 +1,17 @@
-// ---------------------------------- //
+// ------------------------------------- //
 //
 // -- C++ Programming -- Arithmetic Calc --
 //
-// ---------------------------------- //
+// ------------------------------------- //
 
 /*
 Computers are incredible at doing calculations. Now that we have declared 
 Preview: Docs A variable refers to a storage location in the computer’s memory that one can set aside to save, retrieve, and manipulate data. To create a variable, the type must be specified and a value must be assigned to it.
 variables, let’s use them with arithmetic operators to calculate things!
+
+Note: The order of operations can be specified using parentheses. For example, the use of parentheses in score = 5 * (4 + 3) 
+Preview: Docs Loading link description
+sets score equal to 5 * 7 rather than 20 + 3.
 
 */
 
@@ -28,3 +32,4 @@ score = 4 / 2;
 
 score = 5 % 2;
 // and now 1
+
