f44be79ca35bf278be527fc058d29cdf3408a4a8
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-18 00:53:37 +0800

    101 -- C++ Programming -- Arithmetic Compute

--- server-side/codecademy-c++/oct-2024/07-arithmetism-comp.cpp
+++ server-side/codecademy-c++/oct-2024/07-arithmetism-comp.cpp
@@ -10,3 +10,21 @@ Preview: Docs A variable refers to a storage location in the computer’s memory
 variables, let’s use them with arithmetic operators to calculate things!
 
 */
+
+int score = 0;
+// score is 0
+
+score = 4 + 2;
+// it is now 6
+
+score = 4 - 2;
+// it is now 2
+
+score = 4 * 2;
+// it is now 8
+
+score = 4 / 2;
+// and now 2
+
+score = 5 % 2;
+// and now 1
