74faf18cb8c5d1b1be7b99f92bb7dfbb955596be
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-21 22:14:47 +0800

    101 -- C++ Programming -- User Input

--- server-side/codecademy-c++/oct-2024/09-user-input.cpp
+++ server-side/codecademy-c++/oct-2024/09-user-input.cpp
@@ -4,3 +4,9 @@
 //
 // -------------------------------------------- //
 
+/*
+Like we mentioned in the introduction, another way to assign a value to a variable is through user input. A lot of times, we want the user of the program to enter information for the program.
+
+We have cout for output, and there is something called cin that’s used for input!
+
+*/
