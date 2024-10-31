9657230c57e656acb54f0ce12f485781fb3275c9
Abraham Immanuel R. Bormate <abe.bormate@gmail.com>
2024-11-01 02:17:43 +0800

    101 -- C++ Programming -- Input Stream

--- server-side/codecademy-c++/oct-2024/09-user-input.cpp
+++ server-side/codecademy-c++/oct-2024/09-user-input.cpp
@@ -31,6 +31,25 @@ int main() {
   std::cout << "Enter tip amount: ";
   std::cin >> tip;
   
+}
+
+// ------------------------------------------------------------ //
+
+/* 
+
+With chaining --> produce this output --> You paid [tip] dollars.
+
+*/
+
+#include <iostream>
+
+int main() {
   
+  int tip = 0;
+  
+  std::cout << "Enter tip amount: ";
+  std::cin >> tip;
+  
+  std::cout << "You paid " << tip << " dollars. \n";
   
 }
